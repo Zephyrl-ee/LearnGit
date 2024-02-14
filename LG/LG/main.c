575feeaa4f95166c104e083c9dbc02a0de0b1f29
Zephyr Lee <3083567742@qq.com>
2024-02-14 16:38:11 +0800

    First Commit

---
+++ LG/LG/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LG
+//
+//  Created by Zephyr on 2024/2/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
